5d28365d7badb3a6b914fc4ebe14fac7b458a37d
hetvi savsani <savsanihetvi911@gmail.com>
2026-06-12 21:12:00 +0530

    Basic LargestNumber.java

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,7 +1,7 @@
 public class LargestNumber {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
+        int a = 50;
+        int b = 30;
 
         if (a > b) {
             System.out.println(a + " is Largest");
