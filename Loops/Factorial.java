1f3f7504b8ec98f9c8a2dd992a4dbe5cc95ba7ed
hetvi savsani <savsanihetvi911@gmail.com>
2026-06-13 16:46:10 +0530

    Factorial.java

---
+++ Loops/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 12;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+}
